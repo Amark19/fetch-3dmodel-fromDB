3f0eac67f1c77b240d11ba81fef62dbf3406d967
Amark19 <amarkhamkar6@gmail.com>
2022-11-06 00:33:02 +0530

    few changes

--- Assets/scripts/downloadModel.cs
+++ Assets/scripts/downloadModel.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class placemodel : MonoBehaviour
+public class downloadModel : MonoBehaviour
 {
 
     public string url;
     public string name_model ; 
     // Start is called before the first frame update
-    void Start()
+
+    void Start(){
+        // if(name_model){
+        //     Debug.Log("present");
+        // }
+    }
+    public void DownloadModel()
     {
         StartCoroutine(this.GetComponent<loadassets>().webReq(url,name_model));
     }
